b39f8bd0bf4f72ff5bc8795731330fd749363938
Nicxes <nicxesh@gmail.com>
2019-07-05 19:49:00 -0300

    Removing landing code

--- components/navbar.js
+++ components/navbar.js
@@ -3,48 +3,10 @@ import Link from 'next/link'
 
 export default () => (
   <nav className="navbar">
-    <Logo color="white" size="80"/>
 
-    <ul>
-      <li><Link href="/"><a>home</a></Link></li>
-      <li><Link href="/"><a>resume</a></Link></li>
-      <li><Link href="/"><a>login</a></Link></li>
-    </ul>
 
     <style jsx>{`
-      .navbar {
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        width: 100%;
-
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-      .navbar ul {
-        padding: 0;
-        margin: 0;
-
-        display: flex;
-      }
-      .navbar ul li {
-        list-style: none;
-        padding: 0 10px;
-      }
-      .navbar ul li a {
-        color: #999;
-        font-family: Monospace;
-        text-decoration: none;
-      }
-      .navbar ul li a:hover {color: #fff;}
-
-      @media only screen and (min-width: 769px) {
-        .navbar {top: 50px;}
-        .navbar ul {margin: 10px 0;}
-      }
+     
 
     `}</style>
   </nav>
--- pages/index.js
+++ pages/index.js
@@ -7,40 +7,11 @@ function Home() {
     <>
       <Navbar/>
       <Page>
-        <main className="main">
 
-          <div className="container">
-            <h1>History</h1>
-            <p><span className="date">06/2019:</span> <a href="https://aerolab.now.sh">Coding Challenge</a> — Aerolab's Coding Challenge using anything I want.</p>
-            <p><span className="date">12/2018:</span> <a href="https://blackwatch.now.sh">Blackwatch</a> — A self-hosted personal bot for Discord, friendly design and very simple lightweight bot.</p>
-            <p><span className="date">05/2018:</span> <a href="https://boopix.com">Boopix</a> — A photo marketplace that allows users to print his own photos.</p>
-          </div>
-
-        </main>
       </Page>
 
       <style jsx>{`
-        .main {
-          height: 100vh;
-          overflow: hidden;
 
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-        }
-        .main p {
-          font-family: Monospace;
-          line-height: 20px;
-          margin: 0 0 5px 0;
-        }
-        .main p a {
-          color: #fff;
-          text-decoration: none;
-          border-bottom: 1px solid #333;
-        }
-        .main p .date {
-          color: #845dde;
-        }
       `}</style>
     </>
   )
