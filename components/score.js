7906e29cdae7e8e34f5bcf21466775c57f849934
Nicxes <nicxesh@gmail.com>
2019-08-16 15:24:59 -0300

    Create score.js

---
+++ components/score.js
@@ -0,0 +1,19 @@
+export default function Score({ score }) {
+  let final = score / 2
+  let stars = []
+
+  for (let i = 0; i < 5; i++) {
+    if(final > 0) {
+      stars.push(
+        <i className="zi-rate-star zi-icon-star active"/>
+      )
+    } else {
+      stars.push(
+        <i className="zi-rate-star zi-icon-star-off"/>
+      )
+    }
+    final--
+  }
+
+  return (stars)
+}
