47c7cb8c188e1071661cf057ea97750979303f57
Nicxes <nicxesh@gmail.com>
2019-08-15 21:17:29 -0300

    Adding zeit-ui global full support

--- components/page.js
+++ components/page.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 
-function Page ({ title, description, children }) {
+import '../static/zeit-ui.css'
+
+export default function Page ({ title, description, children }) {
   return (
     <div>
       <Head>
@@ -14,40 +16,11 @@ function Page ({ title, description, children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
         <link rel="shortcut icon" type="image/png" href="/static/favicon.png"/>
-        
-        <link href="https://sf.abarba.me/font.css" rel="stylesheet"/>
       </Head>
 
       {children}
 
       <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-        }
-        body {
-          color: #999;
-          font-family: -apple-system, BlinkMacSystemFont, "SF Text", sans-serif;
-
-          background: #fff;
-          margin: 0;
-        }
-        h1, h2, h3, h4, h5, h6 {
-          color: #222;
-          font-family: -apple-system, BlinkMacSystemFont, "SF Display", sans-serif;
-        }
-        a {
-          color: #222;
-          text-decoration: none;
-        
-          transition: 0.1s all ease-in;
-        }
-        .container {
-          max-width: 420px;
-          margin: 0 auto;
-
-          padding: 0 20px;
-        }
         .container-fluid {
           max-width: 90%;
           margin: 0 auto;
@@ -55,38 +28,13 @@ function Page ({ title, description, children }) {
           padding: 0 20px;
         }
         ul.unstyled {
-          padding: 0;
           margin: 0;
+          padding: 0;
         }
-        ul.unstyled li {
-          list-style: none;
-          margin: 8px 0;
-        }
-        p {
-          margin-top: 0;
-        }
-
-        .mb-1 {margin-bottom: 10px !important;}
-        .mb-2 {margin-bottom: 20px !important;}
-        .mb-4 {margin-bottom: 40px !important;}
-
-        hr {
-          border: 1px solid rgba(55, 53, 47, 0.09);
-          margin: 60px 0;
-        }
-
-        @media only screen and (max-width: 425px) {
-          .hidden-mobile {display: none !important;}
-        }
-        @media only screen and (min-width: 426px) and (max-width: 768px) {
-          .hidden-tablet {display: none !important;}
-        }
-        @media only screen and (min-width: 769px) {
-          .hidden-desktop {display: none !important;}
+        ul.unstyled li::before {
+          content: none;
         }
       `}</style>
     </div>
   )
 }
-
-export default Page
--- pages/index.js
+++ pages/index.js
@@ -7,83 +7,72 @@ export default function Home() {
   return (
     <>
       <Page title="Nicxes — Homepage">
-        <main>
-          <div className="container">
+        <main className="zi-main">
+          <div className="zi-layout">
 
-            <header>
+            <div className="avatar">
               <Logo size="140"/>
-              <h1 className="name">Nicxes</h1>
-            </header>
+            </div>
+          
+            <h1 className="name">Nicxes</h1>
 
-            <ul className="navigation">
-              <li><a href="https://www.notion.so/nicxes/UI-Developer-at-Boopix-66d482f92cd14de8bf3b95ff1b6d7136" target="_blank">💼 Experience</a></li>
-              <li><Link href="/oss"><a>💾 OSS Projects</a></Link></li>
-              <li><Link href="/watch"><a>📺 Watch</a></Link></li>
-              <li><a href="https://www.notion.so/nicxes/Information-2547483a7bca43feb829f311bc8f3b5a" target="_blank">👀 Information</a></li>
+            <ul className="unstyled">
+              <li><a>💼 Experience</a></li>
+              <li><a>💾 OSS Projects</a></li>
+              <li><a>📺 Watch</a></li>
+              <li><a>👀 Information</a></li>
             </ul>
 
-            <footer>
-              <p>Got more questions? <br/> Email me at <a href="mailto:hello@nicxes.com">hello@nicxes.com</a></p>
-            </footer>
-
+            <p className="contact zi-comment">Got more questions? <br/> Email me at <a href="mailto:nicxesh@gmail.com">nicxesh@gmail.com</a></p>
           </div>
         </main>
       </Page>
 
       <style jsx>{`
-        main {
+        .zi-main {
           position: relative;
+          width: 100%;
           height: 100vh;
-          
+
           display: flex;
           align-items: center;
+          
         }
-        main .container {width: 100%;}
+        .zi-layout {
+          display: flex;
+          flex-direction: column;
 
-        header {
-          position: relative;
-          padding: 20px 0;
+          max-width: 420px;
+          min-width: 0;
         }
-        header .name {
-          color: #000;
-          font-size: 36px;
-          font-weight: bold;
-
-          margin: 12px 0 0 0;
+        .name {
+          margin: 10px 0;
         }
-        
-        .navigation {
-          padding: 0;
-          margin: 0 0 20px 0;
-        }
-        .navigation li {
-          list-style: none;
-          transition: 0.1s all ease-in;
-
-          border-radius: 2px;
+        ul li {
+          margin: 0;
           border-top: 1px solid #e4e3e2;
         }
-        .navigation li a {
-          color: #333;
-          display: block;
-          padding: 12px 5px;
-        }
-        .navigation li:hover {background: #e4e3e2;}
+        ul li a {
+          color: #222;
+          font-size: 16px;
 
-        footer p {
-          color: #999;
-          font-size: 15px;
-          font-weight: 300;
-          line-height: 25px;
+          display: block;
+          padding: 10px 5px;
         }
-        footer p a {
-          color: #999;
-          text-decoration: underline;
+        ul li a:hover {
+          background: #eee;
+          text-decoration: none;
+        }
+
+        .contact {
+          font-size: 14px;
         }
 
         @media only screen and (max-width: 425px) {
-          main {max-height: 600px;}
-          header {text-align: center;}
+          .avatar,
+          .name {
+            text-align: center;
+          }
         }
       `}</style>
     </>
